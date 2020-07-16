f0bc4b328cc9423dd8db26ff18b726bbc6460b40
Thilak  G V <33113661+thilakgv24@users.noreply.github.com>
2020-07-16 18:56:50 +0530

    Intersection code

---
+++ src/main/java/com/thilak/practice/Car.java
@@ -0,0 +1,19 @@
+package com.thilak.practice;
+
+public class Car extends Vehicle {
+	int doors;
+	
+	public void print() {
+		super.print();
+		System.out.println(this.colour+this.number);
+	}
+	public static void main(String[] args) {
+		Vehicle v = new Car();
+		v.colour = "white";
+		v.number = 10;
+		v.print();
+		
+		Car c = new Car();
+		c.print();		
+	} 
+}
---
+++ src/main/java/com/thilak/practice/Vehicle.java
@@ -0,0 +1,13 @@
+package com.thilak.practice;
+
+public class Vehicle {
+	String colour;
+	int number;
+
+	
+	public void print() {
+		System.out.println(this.colour+this.number);
+	}
+	
+	
+}
